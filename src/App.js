f618385b222bc4c5c33c2f43916c1155c8e2101e
Machineman32 <113127218+Machineman32@users.noreply.github.com>
2022-10-26 22:15:32 -0700

    demo-commit
    This is the unfinished version of the app. Giving the fact that the app is 2 years old, I have to add some new features and also modify my CSS code

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './Appp.css';
+
+import Navbar from './components/Navbar';
+
+import About from './pages/About';
+import Categories from './pages/Categories';
+import ErrorBoundary from './pages/ErrorBoundary';
+import Home from './pages/Home'
+import Login from './pages/Login';
+import SpotifyCallback from './pages/SpotifyCallback';
+import Tracks from './pages/Tracks';
+
+function App() {
+  return (
+          <div className="App">
+              <ErrorBoundary>
+                  <Navbar
+                  />
+                  <div className="mid-wrapper">
+                          <Switch>
+                              <Route
+                                  path="//"
+                                  component={Home}
+                              />
+                              <Route
+                                  path="/about"
+                                  component={About}
+                              />
+                              <Route
+                                  path="/login"
+                                  component={Login}
+                              />
+                              <Route
+                                  path="/categories*"
+                                  component={Categories}
+                              />
+                              <Route
+                                  path="/playlists/:id"
+                                  component={Tracks}
+                              />
+                              <Route
+                                  path="/callback"
+                                  component={SpotifyCallback}
+                              />
+                          </Switch>
+                  </div>
+              </ErrorBoundary>
+          </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Link} from "react-router-dom";
+import './Home.css'
+
+class Home extends React.Component {
+
+    render() {
+        return(
+                <div className="get-started">
+                    <h1>mySpotify</h1>
+                    <p>It can be ourSpotify only if you</p>
+                    <div className="box">
+                        <div className="btn btn-one">
+                            <Link to="/login">
+                                <button className="btn btn-two">Get started</button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+        )
+    }
+}
+
+
+export default Home
